3c3c8330ed5b8b6ec6c5abe477757dc3bab0f025
adnanali8648 <adnan.ali8648@gmail.com>
2023-06-04 22:30:04 +0200

    initial setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Match Scoreboard', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const divElement = screen.getByText(/Start Match/i);
+  expect(divElement).toBeInTheDocument();
 });
